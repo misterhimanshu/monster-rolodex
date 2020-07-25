de0957c31ab80a60a9b84005d180a2d7863c423e
Himanshu Verma <himanshuverma619@gmail.com>
2020-07-25 11:30:02 +0530

    Updates

---
+++ precache-manifest.f6450b502d21aaa4b3bedc71f061f85d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5383c484c9327a892ee52a956bd7fe14",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "4566510260d28efcd9a8",
+    "url": "/monster-rolodex/static/css/main.3707a3c9.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monster-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4566510260d28efcd9a8",
+    "url": "/monster-rolodex/static/js/main.c35b0fc3.chunk.js"
+  },
+  {
+    "revision": "8575afebb5261fef1571",
+    "url": "/monster-rolodex/static/js/runtime-main.502e2e68.js"
+  }
+]);
